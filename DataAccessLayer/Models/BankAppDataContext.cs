e0fd3339229eb6152a9de9998bf2801ddf433ef9
Emma Lübke <lubke.emma@gmail.com>
2025-03-11 16:51:15 +0100

    Added a bootstrap template and changed maincolor to test.

--- DataAccessLayer/Models/BankAppDataContext.cs
+++ DataAccessLayer/Models/BankAppDataContext.cs
@@ -38,7 +38,7 @@ public partial class BankAppDataContext : IdentityDbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        base.OnModelCreating(modelBuilder);
+        base.OnModelCreating(modelBuilder); 
 
         modelBuilder.UseCollation("SQL_Latin1_General_CP1_CI_AS");
 
