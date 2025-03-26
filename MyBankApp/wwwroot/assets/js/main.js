97136f5cf27d85cb8a0e4d23f6c3fc48f23271e3
Emma Lübke <lubke.emma@gmail.com>
2025-03-26 17:39:56 +0100

    Enhance customer details layout
    Restructured `Customer.cshtml` for a Bootstrap-based layout with organized customer details and responsive accounts table.

--- MyBankApp/wwwroot/assets/js/main.js
+++ MyBankApp/wwwroot/assets/js/main.js
@@ -216,3 +216,23 @@
   document.addEventListener('scroll', navmenuScrollspy);
 
 })();
+/** 
+ Search form
+ */
+document.addEventListener("DOMContentLoaded", function () {
+    const searchContainer = document.querySelector(".search-container");
+    const searchIcon = document.querySelector(".search-icon");
+    const searchInput = document.querySelector(".search-input");
+
+    searchIcon.addEventListener("click", function (event) {
+        event.stopPropagation();
+        searchContainer.classList.add("active"); 
+        searchInput.focus(); 
+    });
+
+    document.addEventListener("click", function (event) {
+        if (!searchContainer.contains(event.target)) {
+            searchContainer.classList.remove("active");
+        }
+    });
+});
