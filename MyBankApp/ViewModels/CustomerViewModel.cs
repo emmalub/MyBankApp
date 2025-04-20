0b68a2babc26cc162f0f378a1de0fe52a4c5b202
Emma Lübke <lubke.emma@gmail.com>
2025-04-20 11:45:15 +0200

    Update form validation and labels in Create.cshtml
    Enhanced form labels for clarity and added validation spans
    for email, street address, zip code, city, and country fields
    in `Create.cshtml`.
    
    Updated `CustomerViewModel.cs` with new data annotations
    to enforce required fields for Givenname, Surname, City,
    Zipcode, Country, Streetaddress, and Gender.

--- MyBankApp/ViewModels/CustomerViewModel.cs
+++ MyBankApp/ViewModels/CustomerViewModel.cs
@@ -1,20 +1,36 @@
 ﻿using DataAccessLayer.DTOs;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace MyBankApp.ViewModels
 {
     public class CustomerViewModel
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Set First Name.")]
         public string Givenname { get; set; }
+
+        [Required(ErrorMessage = "Set Last Name.")]
         public string Surname { get; set; }
+
+        [Required(ErrorMessage = "Set City.")]
         public string City { get; set; }
+
+        [Required(ErrorMessage = "Set Zipcode.")]
         public string Zipcode { get; set; }
+
+        [Required(ErrorMessage = "Set Country.")]
         public string Country { get; set; }
         public string Phone { get; set; }
         public string Telephonecountrycode { get; set; }
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Set Streetaddress.")]
         public string Streetaddress { get; set; }
         public int Age { get; set; }
+
+        [Required(ErrorMessage = "Set Gender.")]
         public string Gender { get; set; }
         public string NationalId { get; set; }
         public string Name { get; set; }
